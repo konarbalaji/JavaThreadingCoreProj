998351e8af98c77f69c7be7c836221a101706d48
konarbalaji <konar.balaji@gmail.com>
2023-05-12 11:06:39 +0530

    updated code

--- src/main/java/thread/creation/example/Main1.java
+++ src/main/java/thread/creation/example/Main1.java
@@ -2,8 +2,20 @@ package thread.creation.example;
 
 public class Main1 {
     public static void main(String[] args) {
+
         Thread thread = new Thread(() -> {
             System.out.println("We are now in Thread : [" + Thread.currentThread().getName() + "]");
+            System.out.println("PRIORITY : " + Thread.currentThread().getPriority());
+            throw new RuntimeException("Intentional error");
+        });
+
+        thread.setName("New Worker Thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happened in thread : [" + t.getName() + "] . The error is : " + e.getMessage());
+            }
         });
 
         System.out.println("We are in Thread : [" + Thread.currentThread().getName() + "] before starting a new thread");
